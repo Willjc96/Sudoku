995ce225fc6882f8618f2b164036a4ab6425639c
stevenwilliams <stevo5959@hotmail.co.uk>
2021-03-22 14:01:23 +0000

    Alter grid layout and add container div

--- sudoku-game/src/Components/Grid.jsx
+++ sudoku-game/src/Components/Grid.jsx
@@ -1,19 +1,179 @@
 import React from "react";
 import styled from "styled-components";
 
-export default function Title() {
-	const GridTemplate = styled.div`
-		/* box-sizing: content-box; */
-		border: 3px solid black;
-		border-right: 2px solid black;
-		margin: auto;
-		width: 540px;
-		height: 540px;
-		display: grid;
-		grid-template: repeat(3, 60px) / repeat(3, 60px);
-		border-color: blue;
-		background-color: blanchedalmond;
-	`;
 
-	return <GridTemplate />;
+
+const OuterDiv = styled.div`
+border: solid black 6px;
+width: 540px;
+height: 540px;
+display: grid;
+  grid-template: repeat(3, 1fr) / repeat(3, 1fr);
+display: grid;
+`
+const InnerDiv = styled.div`
+border: 1.5px solid black;
+`
+const ContainerDivOne = styled.div`
+display: grid;
+  grid-template: repeat(3, 1fr) / repeat(3, 1fr);
+  border: 3px solid black;
+`
+const ContainerDivTwo = styled.div`
+display: grid;
+  grid-template: repeat(3, 1fr) / repeat(3, 1fr);
+  border: 3px solid black;
+`
+const ContainerDivThree = styled.div`
+display: grid;
+  grid-template: repeat(3, 1fr) / repeat(3, 1fr);
+  border: 3px solid black;
+`
+const ContainerDivFour = styled.div`
+display: grid;
+  grid-template: repeat(3, 1fr) / repeat(3, 1fr);
+  border: 3px solid black;
+`
+const ContainerDivFive = styled.div`
+display: grid;
+  grid-template: repeat(3, 1fr) / repeat(3, 1fr);
+  border: 3px solid black;
+`
+const ContainerDivSix = styled.div`
+display: grid;
+  grid-template: repeat(3, 1fr) / repeat(3, 1fr);
+  border: 3px solid black;
+`
+const ContainerDivSeven = styled.div`
+display: grid;
+  grid-template: repeat(3, 1fr) / repeat(3, 1fr);
+  border: 3px solid black;
+`
+const ContainerDivEight = styled.div`
+display: grid;
+  grid-template: repeat(3, 1fr) / repeat(3, 1fr);
+  border: 3px solid black;
+`
+const ContainerDivNine = styled.div`
+display: grid;
+  grid-template: repeat(3, 1fr) / repeat(3, 1fr);
+  border: 3px solid black;
+`
+
+export default function Title() {
+	return (
+    <OuterDiv id='outer'>
+      <ContainerDivOne>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    </ContainerDivOne>
+    <ContainerDivTwo>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    </ContainerDivTwo>
+    <ContainerDivThree>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    </ContainerDivThree>
+    <ContainerDivFour>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    </ContainerDivFour>
+    <ContainerDivFive>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    </ContainerDivFive>
+    <ContainerDivSix>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    </ContainerDivSix>
+    <ContainerDivSeven>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    </ContainerDivSeven>
+    <ContainerDivEight>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    </ContainerDivEight>
+    <ContainerDivNine>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    <InnerDiv></InnerDiv>
+    </ContainerDivNine>
+  </OuterDiv>
+  )
 }
+
+
+	// const GridTemplate = styled.div`
+	// 	/* box-sizing: content-box; */
+	// 	border: 3px solid black;
+	// 	border-right: 2px solid black;
+	// 	margin: auto;
+	// 	display: grid;
+	// 	grid-template: repeat(3, 1fr) / repeat(3, 1fr);
+	// 	border-color: blue;
+	// 	background-color: blanchedalmond;
+	// `;
