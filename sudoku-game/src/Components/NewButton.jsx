a4307e28ff4a258136f1a2c27906aadf32dfe7d8
stevenwilliams <stevo5959@hotmail.co.uk>
2021-03-29 13:41:44 +0100

    Remove unnecessary styles and add dropdown to the 'new' button

--- sudoku-game/src/Components/NewButton.jsx
+++ sudoku-game/src/Components/NewButton.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 
 export default function Buttons() {
-	const NewButton = styled.button`
+const [menuState, setMenuState] = useState(false)
+
+
+const changeMenuState = (e) => {
+  setMenuState(!menuState)
+}
+
+  const NewButton = styled.button`
 		color: white;
 		background-color: blue;
 		margin: 1%;
 	`;
+  const DropdownButton = styled.button``
 
-	return <NewButton>New</NewButton>;
+  const MenuDropdownContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  `
+
+  return <NewButton onClick={changeMenuState}>
+    New
+    { menuState ?
+    <MenuDropdownContainer className="menu">
+      <DropdownButton> Easy </DropdownButton>
+      <DropdownButton> Medium </DropdownButton>
+      <DropdownButton> Hard </DropdownButton>
+    </MenuDropdownContainer> : null
+    }
+  </NewButton>;
 }
